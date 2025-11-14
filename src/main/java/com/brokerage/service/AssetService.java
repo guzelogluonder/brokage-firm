2eb3dffb6bbb67e7da05ba22fedecdd2f0406456
guzelogluonder <guzelogluonder@gmail.com>
2025-11-15 00:42:20 +0300

    Changes to the service class have been completed.

--- src/main/java/com/brokerage/service/AssetService.java
+++ src/main/java/com/brokerage/service/AssetService.java
@@ -1,10 +1,27 @@
 package com.brokerage.service;
 
+import com.brokerage.model.Asset;
+import com.brokerage.repository.AssetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AssetService {
+
+    private final AssetRepository assetRepository;
+
+    public List<Asset> getAssetById(@PathVariable UUID id) {
+        return assetRepository.findAllById(Collections.singleton(id));
+    }
+
+    public List<Asset> getAllAsset(){
+        return assetRepository.findAll();
+    }
 
 }
--- src/main/java/com/brokerage/service/OrderService.java
+++ src/main/java/com/brokerage/service/OrderService.java
@@ -1,16 +1,30 @@
 package com.brokerage.service;
 
+import com.brokerage.dto.AssetDto;
 import com.brokerage.dto.OrderDto;
 import com.brokerage.enums.Side;
 import com.brokerage.enums.Status;
+import com.brokerage.mapper.OrderMapper;
+import com.brokerage.model.Order;
+import com.brokerage.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
-    public OrderDto createNewOrder(OrderDto order){
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+    @Transactional
+    public OrderDto createNewOrder(OrderDto order) {
 
         OrderDto newOrder = new OrderDto();
 
@@ -18,11 +32,26 @@ public class OrderService {
                 .customerId(order.getCustomerId())
                 .orderSide(order.getOrderSide())
                 .assetName(order.getAssetName())
+                .size(order.getSize())
                 .price(order.getPrice())
-                .orderSide(Side.BUY)
                 .status(Status.PENDING)
+                .createDate(LocalDateTime.now())
                 .build();
-        return newOrder;
+
+       Order savedOrder = orderRepository.save(orderMapper.orderDtoToOrder(newOrder));
+
+        return orderMapper.orderToDto(savedOrder);
+    }
+
+    public List<Order> getOrder(UUID customerId) {
+
+        List<Order> orders = orderRepository.findAll();
+
+        return orders.stream().filter(order -> order.getCustomerId().equals(customerId)).toList();
+    }
+
+    public void deleteOrder(Status status,UUID customerId, UUID orderId) {
+         orderRepository.deleteById(status,orderId,customerId);
     }
 
 }
