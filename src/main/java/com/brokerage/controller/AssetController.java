b45cd534bb88ed5340deabbb055e49ccd1a1f4eb
guzelogluonder <guzelogluonder@gmail.com>
2025-11-15 00:46:01 +0300

    added get and post requests for create order, list order, delete order and list assets

--- src/main/java/com/brokerage/controller/AssetController.java
+++ src/main/java/com/brokerage/controller/AssetController.java
@@ -1,4 +1,33 @@
 package com.brokerage.controller;
 
+import com.brokerage.model.Asset;
+import com.brokerage.service.AssetService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
 public class AssetController {
+
+    private final AssetService assetService;
+
+    public static final String ASSET_PATH = "/assets";
+    public static final String ASSET_PATH_ID = "/assets/{customerId}";
+
+    @GetMapping(ASSET_PATH_ID)
+    public List<Asset> getAsset(@PathVariable("customerId") UUID customerId){
+        return ResponseEntity.ok().body(assetService.getAssetById(customerId)).getBody();
+    }
+
+    @GetMapping(ASSET_PATH)
+    public Iterable<Asset> getAllAsset(){
+        return ResponseEntity.ok().body(this.assetService.getAllAsset()).getBody();
+    }
+
 }
--- src/main/java/com/brokerage/controller/OrderController.java
+++ src/main/java/com/brokerage/controller/OrderController.java
@@ -1,27 +1,45 @@
 package com.brokerage.controller;
 
 import com.brokerage.dto.OrderDto;
+import com.brokerage.enums.Status;
+import com.brokerage.model.Order;
 import com.brokerage.service.OrderService;
 import lombok.AllArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
 @Slf4j
 @AllArgsConstructor
 @RestController
 public class OrderController {
 
     private final OrderService orderService;
-    @PostMapping("/orders")
-    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto){
 
-      OrderDto newOrder = orderService.createNewOrder(orderDto);
+    public static final String ORDER_CREATE_PATH = "/orders/{customerId}";
+    public static final String DELETE_ORDER_CREATE_PATH = "/orders/{customerId}/{orderId}";
 
-        return ResponseEntity.ok().body(newOrder);
+    @PostMapping(ORDER_CREATE_PATH)
+    public ResponseEntity<OrderDto> createOrder(@PathVariable("customerId") UUID customerId, @RequestBody OrderDto orderDto){
 
+        orderDto.setCustomerId(customerId);
+
+        return ResponseEntity.ok().body( orderService.createNewOrder(orderDto));
+
+    }
+
+    @GetMapping(ORDER_CREATE_PATH)
+    public List<Order> getOrder(@PathVariable("customerId") UUID customerId){
+        return ResponseEntity.ok().body(orderService.getOrder(customerId)).getBody();
+    }
+
+    @DeleteMapping(DELETE_ORDER_CREATE_PATH)
+    public void deleteOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+        orderService.deleteOrder(Status.CANCELED,customerId,orderId);
     }
 
 }
