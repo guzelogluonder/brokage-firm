c0181f26c684e724f0169f4cfad2992b2252d699
guzelogluonder <guzelogluonder@gmail.com>
2025-11-17 21:00:04 +0300

    match functionality added for admin user.

--- src/main/java/com/brokerage/controller/AdminController.java
+++ src/main/java/com/brokerage/controller/AdminController.java
@@ -1,15 +1,26 @@
 package com.brokerage.controller;
 
-import com.brokerage.service.AssetService;
+import com.brokerage.dto.OrderDto;
+import com.brokerage.service.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @AllArgsConstructor
 @RestController
 public class AdminController {
-    private final AssetService assetService;
+    private final OrderService orderService;
+
+    @PostMapping("/api/match-order")
+    public ResponseEntity<OrderDto> matchOrder(@RequestBody OrderDto order) {
+        OrderDto response = orderService.matchOrder(order.getOrderId());
+        return ResponseEntity.ok(response);
+}
+
 
 }
--- src/main/java/com/brokerage/service/OrderService.java
+++ src/main/java/com/brokerage/service/OrderService.java
@@ -6,7 +6,6 @@ import com.brokerage.enums.Status;
 import com.brokerage.exception.InsufficientBalanceException;
 import com.brokerage.exception.InvalidOrderStatusException;
 import com.brokerage.exception.ResourceNotFoundException;
-import com.brokerage.mapper.AssetMapper;
 import com.brokerage.mapper.OrderMapper;
 import com.brokerage.model.Asset;
 import com.brokerage.model.Order;
@@ -48,7 +47,7 @@ public class OrderService {
             handleSellAsset(newOrder);
         }
 
-     Order savedOrder = orderRepository.save(orderMapper.orderDtoToOrder(newOrder));
+        Order savedOrder = orderRepository.save(orderMapper.orderDtoToOrder(newOrder));
 
         return orderMapper.orderToDto(savedOrder);
     }
@@ -124,4 +123,84 @@ public class OrderService {
         asset.setUsableSize(asset.getUsableSize().add(order.getSize()));
         assetRepository.save(asset);
     }
+
+    public OrderDto matchOrder(UUID orderId) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found with ID: " + orderId));
+
+        if (order.getStatus() != Status.PENDING) {
+            throw new InvalidOrderStatusException("Only PENDING orders can be matched. Current status: " + order.getStatus());
+        }
+        if (order.getOrderSide() == Side.BUY) {
+            matchBuyOrder(orderMapper.orderToDto(order));
+        } else {
+            matchSellOrder(orderMapper.orderToDto(order));
+        }
+
+        order.setStatus(Status.MATCHED);
+        orderRepository.save(order);
+
+        return orderMapper.orderToDto(order);
+    }
+
+    private void matchBuyOrder(OrderDto order) {
+        BigDecimal totalPrice = order.getSize().multiply(order.getPrice());
+
+        Asset tryAsset = assetRepository.findByCustomerIdAndAssetName(order.getCustomerId(), "TRY")
+                .orElseThrow(() -> new ResourceNotFoundException("Asset TRY not found for customer: " + order.getCustomerId()));
+
+        // try asset size is deducted
+        tryAsset.setSize(tryAsset.getSize().subtract(totalPrice));
+
+        assetRepository.save(tryAsset);
+
+       Asset stockAsset = assetRepository.findByCustomerIdAndAssetName(order.getCustomerId(), order.getAssetName())
+               .orElseGet(() -> {
+                   Asset newAsset = new Asset();
+                   newAsset.setCustomerId(order.getCustomerId());
+                   newAsset.setAssetName(order.getAssetName());
+                   newAsset.setSize(BigDecimal.ZERO);
+                   newAsset.setUsableSize(BigDecimal.ZERO);
+                   return newAsset;
+               });
+
+       stockAsset.setSize(stockAsset.getSize().add(order.getSize()));
+       stockAsset.setUsableSize(stockAsset.getUsableSize().add(order.getSize()));
+
+       assetRepository.save(stockAsset);
+
+
+
+    }
+    private void matchSellOrder(OrderDto order) {
+        BigDecimal totalPrice = order.getSize().multiply(order.getPrice());
+
+        Asset stockAsset = assetRepository.findByCustomerIdAndAssetName(order.getCustomerId(), order.getAssetName())
+                .orElseThrow(() -> new ResourceNotFoundException("Stock asset not found for customer: " + order.getCustomerId()));
+
+        // Stock asset size is deducted
+        stockAsset.setSize(stockAsset.getSize().subtract(order.getSize()));
+
+        assetRepository.save(stockAsset);
+
+       Asset tryAsset = assetRepository.findByCustomerIdAndAssetName(order.getCustomerId(), "TRY")
+               .orElseGet(() -> {
+                   Asset newTryAsset = new Asset();
+                   newTryAsset.setCustomerId(order.getCustomerId());
+                   newTryAsset.setAssetName("TRY");
+                   newTryAsset.setSize(BigDecimal.ZERO);
+                   newTryAsset.setUsableSize(BigDecimal.ZERO);
+                   return newTryAsset;
+               });
+
+        tryAsset.setSize(tryAsset.getSize().add(totalPrice));
+        tryAsset.setUsableSize(tryAsset.getUsableSize().add(totalPrice));
+
+       assetRepository.save(tryAsset);
+
+
+
+    }
+
 }
